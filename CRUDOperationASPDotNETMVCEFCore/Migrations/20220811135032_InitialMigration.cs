ac07472c76ae0b0021839d2985a362576a564d07
USER <USER@DESKTOP-8UQ58DI>
2022-08-11 15:04:14 +0100

    Add project files.

---
+++ CRUDOperationASPDotNETMVCEFCore/Migrations/20220811135032_InitialMigration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRUDOperationASPDotNETMVCEFCore.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Teachers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Skills = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TotalStudent = table.Column<int>(type: "int", nullable: false),
+                    Salary = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    AddedOn = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teachers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Teachers");
+        }
+    }
+}
---
+++ CRUDOperationASPDotNETMVCEFCore/Models/TeacherContex.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUDOperationASPDotNETMVCEFCore.Models
+{
+    public class TeacherContex : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public TeacherContex(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to sql server with connection string from app settings
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultDatabase"));
+        }
+        public DbSet<Teacher> Teachers { get; set; }
+
+    }
+}
